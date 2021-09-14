721e4ad33368b2ae55b335266df2903bac09bd6b
Paolo Cargnin <paolocargnin@boom.ink>
2021-09-14 10:58:31 +0200

    check

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code of my exstenssion
+chrome.runtime.onInstalled.addListener(() =>  {
+    // 1 - SAVING something on "chrome.storage"
+    // chrome.storage.sync.set({color: '#3aa757'}, () {
+    //   console.log('The color is green.');
+    // });
+  
+    // 2 - Making the popup avaiable
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+          // pageUrl: {hostEquals: 'developer.chrome.com'},
+        })
+        ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+    });
+  
+  });
